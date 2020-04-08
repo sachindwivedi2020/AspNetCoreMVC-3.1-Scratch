baad2ce0141ccee5e89d33baabd5831b490ad98f
Sachin Dwivedi <Sachin Dwivedi@DESKTOP-GLHDG6M>
2020-04-09 01:45:19 +0530

    Added Razor file compilation i.e Runtime compilation

--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -18,6 +18,9 @@ namespace BookStore
         {
             //Add Dependency that you going to add MVC 
             services.AddControllersWithViews();
+#if DEBUG
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
