fb4c8630af9ddb8d9065717e7c3af1c95447e738
Sachin Dwivedi <Sachin Dwivedi@DESKTOP-GLHDG6M>
2020-04-09 17:06:49 +0530

    Add GetBook View Means Design and develop a product detail page

--- BookStore/BookStore/Controllers/BookController.cs
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -20,9 +20,11 @@ namespace BookStore.Controllers
             var data= _bookRepository.GetAllBooks();
             return View(data);
         }
-        public BookModel GetBook(int id)
+        public ViewResult GetBook(int id)
         {
-            return _bookRepository.GetBookById(id);        }
+            var data= _bookRepository.GetBookById(id);
+            return View(data);
+        }
 
         public List<BookModel> SearchBook(string bookName ,string authorName)
         {
